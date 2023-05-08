2d9307a5d5db1f6140c8fbe76a2d7aa5a0950533
Noah S <Noah3141@gmail.com>
2023-05-07 20:17:26 -0400

    Tailwind initialized

---
+++ app-frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,rs}", "./index.html"], // Look here for Tailwind to unpack!
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
